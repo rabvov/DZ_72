976f31de201c78e4e766fe94db5ba148468b3b0f
rak15 <rak1529916@mail.ru>
2023-04-25 14:10:46 +0300

    1.1

--- src/DZ_72.java
+++ src/DZ_72.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DZ_72 {
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class DZ_72 {
+    public static void main(String[] args) {
+        String x,y;
+
+        Scanner scanner = new Scanner(System.in);
+        x=scanner.next();
+        y=scanner.next();
+        scanner.close();
+
+        System.out.println(x.length()+" "+y.length());
+        System.out.printf("x=%d; y=%d",x.length(),y.length());
+
+
+
+
+
+
+    }
 }
